1dce7b07cc437ac3cb1606effc0c71d6ab48bb64
Allan <allancamargodeoliveira41@gmail.com>
2025-05-29 20:25:21 -0300

    api sla oq deu aqui

--- bim2/05-maio/atividade_api/script.js
+++ bim2/05-maio/atividade_api/script.js
@@ -1,9 +1,25 @@
-const isbn = document.getElementById("isbn").value
-const url = `https://brasilapi.com.br/api/isbn/v1/${isbn}`
+const isbn = document.getElementById("isbn").value;
+const url = `https://brasilapi.com.br/api/isbn/v1/${isbn}`;
 
-function buscarLivro(){
-    fetch(url)
-        .then((resposta)=>{return resposta.json()})
-        .then((dados))
-
+function buscarLivro() {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.name) {
+        alert("ISBN não encontrado.");
+        return;
+      }
+      document.getElementById("titulo").value = data.title;
+      document.getElementById("autor").value = data.authors;
+      document.getElementById("publicadora").value = data.publisher;
+      document.getElementById("ano").value = data.year;
+      document.getElementById("paginas").value = data.page_count;
+      document.getElementById("localizacao").value = data.location;
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Erro ao buscar o ISBN. Tente novamente mais tarde.");
+    });
 }
