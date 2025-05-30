676ce16b394179376de63590fef6057ad5603059
Allan <153043227+All4mn@users.noreply.github.com>
2025-05-30 10:00:28 -0300

    react

---
+++ bim2/05-maio/intro_react/src/header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+function Header(){
+    return(
+        <header className="app-header">
+            <h1>Bem-vindo Chefe!</h1>
+        </header>
+    )
+}
+
+export default Header;
