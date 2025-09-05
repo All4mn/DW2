00370f37f3cb5d59024ba8c6b5643a2364695b7a
Allan <153043227+All4mn@users.noreply.github.com>
2025-09-05 09:28:33 -0300

    a

--- bim3/o9-setembro/contador/src/App.jsx
+++ bim3/o9-setembro/contador/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
-import './App.css'
 import './styles/global.css'
 import './styles/theme.css'
 
 function App() {
-  return{
-    
-  }
+  return(
+    <div class="container">
+      <h1>Contador</h1>
+      <div class="counter-display" id="counter">0</div>
+      <div class="buttons">
+        <button class="btn-decrease" onclick="decreaseCounter()">-</button>
+        <button class="btn-increase" onclick="increaseCounter()">+</button>
+      </div>
+      <button class="btn-reset" onclick="resetCounter()">Reset</button>
+    </div>
+  )
 }
 export default App
