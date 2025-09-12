0684ceaac4d59aa66ab2125eb3fb4c78362812e8
Allan <153043227+All4mn@users.noreply.github.com>
2025-09-12 08:55:52 -0300

    css

--- bim3/o9-setembro/contador/src/components/contador.jsx
+++ bim3/o9-setembro/contador/src/components/contador.jsx
@@ -1,15 +1,37 @@
-import styles from "./contador.module.css"
+import styles from "./contador.module.css";
 
-export default function Contador(){
-    return(
-        <div className={styles.container}>
-          <h1>Contador</h1>
-          <div >0</div>
-          <div >
-            <button >-</button>
-            <button >+</button>
-          </div>
-          <button >Reset</button>
-        </div>
-      )
+import { useState } from "react";
+
+export default function Contador() {
+  const [contador, setContador] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function incrementar() {
+    setContador((contador) => contador + step);
+  }
+  function decrementar() {
+    setContador((contador) => contador - step);
+  }
+  function reset() {
+    setContador(0);
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1>Contador</h1>
+      <div className={styles.contadorDisplay}>{contador}</div>
+      <div className={styles.botaoContainer}>
+        <button className={styles.botao} onClick={decrementar}>
+          -
+        </button>
+        <button className={styles.botao} onClick={incrementar}>
+          +
+        </button>
+      </div>
+      <button className={styles.botao} onClick={reset}>
+        Reset
+      </button>
+      <input type="number" name="step" id="step" value={step} onChange={e => setStep(parseInt(e.target.value))}/>
+    </div>
+  );
 }
