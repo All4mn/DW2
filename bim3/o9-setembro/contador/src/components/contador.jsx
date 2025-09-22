b3256c776dc4d6c8a97dea9fc371aca6b97a71eb
Allan <153043227+All4mn@users.noreply.github.com>
2025-09-22 08:54:49 -0300

    Update contador.jsx

--- bim3/o9-setembro/contador/src/components/contador.jsx
+++ bim3/o9-setembro/contador/src/components/contador.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import styles from "./contador.module.css";
 
 import { useState } from "react";
@@ -15,12 +16,24 @@ export default function Contador() {
   function reset() {
     setContador(0);
   }
+=======
+import styles from "./contador.module.css"
+import { useState } from 'react'
+
+export default function Contador() {
+
+  // const estado = useState(0)
+  // let contador = estado[0]
+  // let setContador = estado[1]
+  const [contador, setContador] = useState(0)
+>>>>>>> Stashed changes
 
   return (
     <div className={styles.container}>
       <h1>Contador</h1>
       <div className={styles.contadorDisplay}>{contador}</div>
       <div className={styles.botaoContainer}>
+<<<<<<< Updated upstream
         <button className={styles.botao} onClick={decrementar}>
           -
         </button>
@@ -32,6 +45,12 @@ export default function Contador() {
         Reset
       </button>
       <input type="number" name="step" id="step" value={step} onChange={e => setStep(parseInt(e.target.value))}/>
+=======
+        <button className={styles.botao} onClick={()=>setContador(contador - 1)}>-</button>
+        <button className={styles.botao} onClick={()=>setContador(contador + 1)}>+</button>
+      </div>
+      <button className={styles.botao} onClick={()=>setContador(0)}> Reset</button>
+>>>>>>> Stashed changes
     </div>
   );
 }
